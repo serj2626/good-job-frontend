9f0f9b33fe48f28b9f4b8ed29f10e85efa735fea
serj2626 <serj2626@mail.ru>
2024-11-15 14:19:31 +0300

    feat: ugrade code

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
     class="relative flex size-full min-h-screen flex-col bg-white group/design-root overflow-x-hidden"
     style="font-family: Manrope, 'Noto Sans', sans-serif"
   >
-    <div class="layout-container flex h-full grow flex-col">
+    <div class="flex h-full grow flex-col">
       <div class="gap-1 px-6 flex flex-1 justify-center py-5">
         <div class="layout-content-container flex flex-col w-80">
           <div
