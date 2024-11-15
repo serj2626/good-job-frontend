867c94ed7d1c300676c7052cda0c770fb1815023
serj2626 <serj2626@mail.ru>
2024-11-15 23:28:21 +0300

    feat: created page for interview

---
+++ pages/interview/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { InterviewPage } from '~/src/pages/interview';
+</script>
+<template>
+  <InterviewPage>Interview Page</InterviewPage>
+</template>
+<style scoped lang="scss"></style>
---
+++ src/pages/interview/index.ts
@@ -0,0 +1,1 @@
+export { default as InterviewPage } from "./ui/index.vue";
