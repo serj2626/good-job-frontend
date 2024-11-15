f1eb2b5bcd8d3df159387c09be4e19b2cbb44d2c
serj2626 <serj2626@mail.ru>
2024-11-15 14:54:04 +0300

    feat: updated config for tailwind and layouts

--- .nuxt/types/layouts.d.ts
+++ .nuxt/types/layouts.d.ts
@@ -1,5 +1,5 @@
 import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "default"
+export type LayoutKey = "company" | "default" | "employee"
 declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
