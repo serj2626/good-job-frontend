088b81b0c734a0f2589070b5cf4ec8f300e227c4
serj2626 <serj2626@mail.ru>
2024-11-15 13:57:35 +0300

    feat: created page for registration client

---
+++ src/pages/auth-registration/index.ts
@@ -0,0 +1,1 @@
+export { default as AuthRegistrationPage } from "./ui/index.vue";
---
+++ src/pages/auth-registration/ui/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>auth-registration</h1>
+    <slot></slot>
+  </div>
+</template>
+<style scoped lang="scss"></style>
