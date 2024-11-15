98cd478d9e468816004140c47378d8fe21c0a53d
serj2626 <serj2626@mail.ru>
2024-11-15 14:53:35 +0300

    feat: added Container component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,164 +1,182 @@
 <script setup lang="ts"></script>
 <template>
-  <div
-    class="relative flex size-full min-h-screen flex-col bg-white group/design-root overflow-x-hidden"
-    style="font-family: Manrope, 'Noto Sans', sans-serif"
-  >
-    <div class="flex h-full grow flex-col">
-      <div class="gap-1 px-6 flex flex-1 justify-center py-5">
-        <div class="layout-content-container flex flex-col w-80">
+  <UContainer>
+  <div class="grid grid-cols-[minmax(0,300px),1fr] h-screen">
+    <div class="left-sidebar">
+      <div class="flex flex-col gap-2 h-full items-center">
+        <div class="flex items-center gap-3 px-3 py-2 rounded-xl bg-[#f0f2f4]">
           <div
-            class="flex h-full min-h-[700px] flex-col justify-between bg-white p-4"
+            class="text-[#111418]"
+            data-icon="House"
+            data-size="24px"
+            data-weight="fill"
           >
-            <div class="flex flex-col gap-4">
-              <div class="flex flex-col gap-2">
-                <div
-                  class="flex items-center gap-3 px-3 py-2 rounded-xl bg-[#f0f2f4]"
-                >
-                  <div
-                    class="text-[#111418]"
-                    data-icon="House"
-                    data-size="24px"
-                    data-weight="fill"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24px"
-                      height="24px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path
-                        d="M224,115.55V208a16,16,0,0,1-16,16H168a16,16,0,0,1-16-16V168a8,8,0,0,0-8-8H112a8,8,0,0,0-8,8v40a16,16,0,0,1-16,16H48a16,16,0,0,1-16-16V115.55a16,16,0,0,1,5.17-11.78l80-75.48.11-.11a16,16,0,0,1,21.53,0,1.14,1.14,0,0,0,.11.11l80,75.48A16,16,0,0,1,224,115.55Z"
-                      ></path>
-                    </svg>
-                  </div>
-                  <p class="text-[#111418] text-sm font-medium leading-normal">
-                    Home
-                  </p>
-                </div>
-                <div class="flex items-center gap-3 px-3 py-2">
-                  <div
-                    class="text-[#111418]"
-                    data-icon="Bookmark"
-                    data-size="24px"
-                    data-weight="regular"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24px"
-                      height="24px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path
-                        d="M184,32H72A16,16,0,0,0,56,48V224a8,8,0,0,0,12.24,6.78L128,193.43l59.77,37.35A8,8,0,0,0,200,224V48A16,16,0,0,0,184,32Zm0,16V161.57l-51.77-32.35a8,8,0,0,0-8.48,0L72,161.56V48ZM132.23,177.22a8,8,0,0,0-8.48,0L72,209.57V180.43l56-35,56,35v29.14Z"
-                      ></path>
-                    </svg>
-                  </div>
-                  <p class="text-[#111418] text-sm font-medium leading-normal">
-                    Saved Jobs
-                  </p>
-                </div>
-                <div class="flex items-center gap-3 px-3 py-2">
-                  <div
-                    class="text-[#111418]"
-                    data-icon="Briefcase"
-                    data-size="24px"
-                    data-weight="regular"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24px"
-                      height="24px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path
-                        d="M216,56H176V48a24,24,0,0,0-24-24H104A24,24,0,0,0,80,48v8H40A16,16,0,0,0,24,72V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V72A16,16,0,0,0,216,56ZM96,48a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96ZM216,72v41.61A184,184,0,0,1,128,136a184.07,184.07,0,0,1-88-22.38V72Zm0,128H40V131.64A200.19,200.19,0,0,0,128,152a200.25,200.25,0,0,0,88-20.37V200ZM104,112a8,8,0,0,1,8-8h32a8,8,0,0,1,0,16H112A8,8,0,0,1,104,112Z"
-                      ></path>
-                    </svg>
-                  </div>
-                  <p class="text-[#111418] text-sm font-medium leading-normal">
-                    Applied Jobs
-                  </p>
-                </div>
-                <div class="flex items-center gap-3 px-3 py-2">
-                  <div
-                    class="text-[#111418]"
-                    data-icon="Buildings"
-                    data-size="24px"
-                    data-weight="regular"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24px"
-                      height="24px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path
-                        d="M240,208H224V96a16,16,0,0,0-16-16H144V32a16,16,0,0,0-24.88-13.32L39.12,72A16,16,0,0,0,32,85.34V208H16a8,8,0,0,0,0,16H240a8,8,0,0,0,0-16ZM208,96V208H144V96ZM48,85.34,128,32V208H48ZM112,112v16a8,8,0,0,1-16,0V112a8,8,0,1,1,16,0Zm-32,0v16a8,8,0,0,1-16,0V112a8,8,0,1,1,16,0Zm0,56v16a8,8,0,0,1-16,0V168a8,8,0,0,1,16,0Zm32,0v16a8,8,0,0,1-16,0V168a8,8,0,0,1,16,0Z"
-                      ></path>
-                    </svg>
-                  </div>
-                  <p class="text-[#111418] text-sm font-medium leading-normal">
-                    Company Directory
-                  </p>
-                </div>
-                <div class="flex items-center gap-3 px-3 py-2">
-                  <div
-                    class="text-[#111418]"
-                    data-icon="Money"
-                    data-size="24px"
-                    data-weight="regular"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24px"
-                      height="24px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path
-                        d="M128,88a40,40,0,1,0,40,40A40,40,0,0,0,128,88Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,128,152ZM240,56H16a8,8,0,0,0-8,8V192a8,8,0,0,0,8,8H240a8,8,0,0,0,8-8V64A8,8,0,0,0,240,56ZM193.65,184H62.35A56.78,56.78,0,0,0,24,145.65v-35.3A56.78,56.78,0,0,0,62.35,72h131.3A56.78,56.78,0,0,0,232,110.35v35.3A56.78,56.78,0,0,0,193.65,184ZM232,93.37A40.81,40.81,0,0,1,210.63,72H232ZM45.37,72A40.81,40.81,0,0,1,24,93.37V72ZM24,162.63A40.81,40.81,0,0,1,45.37,184H24ZM210.63,184A40.81,40.81,0,0,1,232,162.63V184Z"
-                      ></path>
-                    </svg>
-                  </div>
-                  <p class="text-[#111418] text-sm font-medium leading-normal">
-                    Salary
-                  </p>
-                </div>
-                <div class="flex items-center gap-3 px-3 py-2">
-                  <div
-                    class="text-[#111418]"
-                    data-icon="ChatCircle"
-                    data-size="24px"
-                    data-weight="regular"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24px"
-                      height="24px"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <path
-                        d="M128,24A104,104,0,0,0,36.18,176.88L24.83,210.93a16,16,0,0,0,20.24,20.24l34.05-11.35A104,104,0,1,0,128,24Zm0,192a87.87,87.87,0,0,1-44.06-11.81,8,8,0,0,0-6.54-.67L40,216,52.47,178.6a8,8,0,0,0-.66-6.54A88,88,0,1,1,128,216Z"
-                      ></path>
-                    </svg>
-                  </div>
-                  <p class="text-[#111418] text-sm font-medium leading-normal">
-                    Interview
-                  </p>
-                </div>
-              </div>
-            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+            >
+              <path
+                d="M224,115.55V208a16,16,0,0,1-16,16H168a16,16,0,0,1-16-16V168a8,8,0,0,0-8-8H112a8,8,0,0,0-8,8v40a16,16,0,0,1-16,16H48a16,16,0,0,1-16-16V115.55a16,16,0,0,1,5.17-11.78l80-75.48.11-.11a16,16,0,0,1,21.53,0,1.14,1.14,0,0,0,.11.11l80,75.48A16,16,0,0,1,224,115.55Z"
+              ></path>
+            </svg>
           </div>
+          <p class="text-[#111418] text-sm font-medium leading-normal">Главная </p>
         </div>
-
-        <slot></slot>
+        <div class="flex items-center gap-3 px-3 py-2">
+          <div
+            class="text-[#111418]"
+            data-icon="Bookmark"
+            data-size="24px"
+            data-weight="regular"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+            >
+              <path
+                d="M184,32H72A16,16,0,0,0,56,48V224a8,8,0,0,0,12.24,6.78L128,193.43l59.77,37.35A8,8,0,0,0,200,224V48A16,16,0,0,0,184,32Zm0,16V161.57l-51.77-32.35a8,8,0,0,0-8.48,0L72,161.56V48ZM132.23,177.22a8,8,0,0,0-8.48,0L72,209.57V180.43l56-35,56,35v29.14Z"
+              ></path>
+            </svg>
+          </div>
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Избранное
+          </p>
+        </div>
+        <div class="flex items-center gap-3 px-3 py-2">
+          <div
+            class="text-[#111418]"
+            data-icon="Briefcase"
+            data-size="24px"
+            data-weight="regular"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+            >
+              <path
+                d="M216,56H176V48a24,24,0,0,0-24-24H104A24,24,0,0,0,80,48v8H40A16,16,0,0,0,24,72V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V72A16,16,0,0,0,216,56ZM96,48a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96ZM216,72v41.61A184,184,0,0,1,128,136a184.07,184.07,0,0,1-88-22.38V72Zm0,128H40V131.64A200.19,200.19,0,0,0,128,152a200.25,200.25,0,0,0,88-20.37V200ZM104,112a8,8,0,0,1,8-8h32a8,8,0,0,1,0,16H112A8,8,0,0,1,104,112Z"
+              ></path>
+            </svg>
+          </div>
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Мои резюме
+          </p>
+        </div>
+        <div class="flex items-center gap-3 px-3 py-2">
+          <div
+            class="text-[#111418]"
+            data-icon="Buildings"
+            data-size="24px"
+            data-weight="regular"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+            >
+              <path
+                d="M240,208H224V96a16,16,0,0,0-16-16H144V32a16,16,0,0,0-24.88-13.32L39.12,72A16,16,0,0,0,32,85.34V208H16a8,8,0,0,0,0,16H240a8,8,0,0,0,0-16ZM208,96V208H144V96ZM48,85.34,128,32V208H48ZM112,112v16a8,8,0,0,1-16,0V112a8,8,0,1,1,16,0Zm-32,0v16a8,8,0,0,1-16,0V112a8,8,0,1,1,16,0Zm0,56v16a8,8,0,0,1-16,0V168a8,8,0,0,1,16,0Zm32,0v16a8,8,0,0,1-16,0V168a8,8,0,0,1,16,0Z"
+              ></path>
+            </svg>
+          </div>
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Мои вакансии
+          </p>
+        </div>
+        <div class="flex items-center gap-3 px-3 py-2">
+          <div
+            class="text-[#111418]"
+            data-icon="ChatCircle"
+            data-size="24px"
+            data-weight="regular"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+            >
+              <path
+                d="M128,24A104,104,0,0,0,36.18,176.88L24.83,210.93a16,16,0,0,0,20.24,20.24l34.05-11.35A104,104,0,1,0,128,24Zm0,192a87.87,87.87,0,0,1-44.06-11.81,8,8,0,0,0-6.54-.67L40,216,52.47,178.6a8,8,0,0,0-.66-6.54A88,88,0,1,1,128,216Z"
+              ></path>
+            </svg>
+          </div>
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Моя группа
+          </p>
+        </div>
+        <div class="flex items-center gap-3 px-3 py-2">
+          <div
+            class="text-[#111418]"
+            data-icon="Money"
+            data-size="24px"
+            data-weight="regular"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+            >
+              <path
+                d="M128,88a40,40,0,1,0,40,40A40,40,0,0,0,128,88Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,128,152ZM240,56H16a8,8,0,0,0-8,8V192a8,8,0,0,0,8,8H240a8,8,0,0,0,8-8V64A8,8,0,0,0,240,56ZM193.65,184H62.35A56.78,56.78,0,0,0,24,145.65v-35.3A56.78,56.78,0,0,0,62.35,72h131.3A56.78,56.78,0,0,0,232,110.35v35.3A56.78,56.78,0,0,0,193.65,184ZM232,93.37A40.81,40.81,0,0,1,210.63,72H232ZM45.37,72A40.81,40.81,0,0,1,24,93.37V72ZM24,162.63A40.81,40.81,0,0,1,45.37,184H24ZM210.63,184A40.81,40.81,0,0,1,232,162.63V184Z"
+              ></path>
+            </svg>
+          </div>
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Мои отклики
+          </p>
+        </div>
+        <div class="flex items-center gap-3 px-3 py-2">
+          <div
+            class="text-[#111418]"
+            data-icon="ChatCircle"
+            data-size="24px"
+            data-weight="regular"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+            >
+              <path
+                d="M128,24A104,104,0,0,0,36.18,176.88L24.83,210.93a16,16,0,0,0,20.24,20.24l34.05-11.35A104,104,0,1,0,128,24Zm0,192a87.87,87.87,0,0,1-44.06-11.81,8,8,0,0,0-6.54-.67L40,216,52.47,178.6a8,8,0,0,0-.66-6.54A88,88,0,1,1,128,216Z"
+              ></path>
+            </svg>
+          </div>
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Собеседования
+          </p>
+        </div>
       </div>
     </div>
+    <div class="right-sidebar bg-gray-100">
+      <slot></slot>
+    </div>
   </div>
+  </UContainer> 
 </template>
+<style scoped lang="scss">
+li:hover {
+  background-color: #adb7c2;
+  border-radius: 10px;
+  padding: 15px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+</style>
