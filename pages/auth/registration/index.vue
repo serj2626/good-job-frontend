643a7eb2017084fbbf43f4af562963d59c6257b9
serj2626 <serj2626@mail.ru>
2024-11-15 13:58:05 +0300

    feat: added import AuthRegistrationPage

---
+++ pages/auth/registration/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { AuthRegistrationPage } from '~/src/pages/auth-registration';
+</script>
+<template>
+    <AuthRegistrationPage />
+</template>
+<style scoped lang="scss"></style>
