696524292c1ed43f09c44b73767047b31029acda
serj2626 <serj2626@mail.ru>
2024-11-17 01:07:56 +0300

    feat: update layout for company

--- app/layouts/company.vue
+++ app/layouts/company.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { CompanySidebar } from "~/src/widgets/CompanySideBar";
+</script>
 <template>
-<slot></slot>
+    <div class="grid grid-cols-[minmax(0,300px),1fr] h-screen py-12">
+      <div class="left-sidebar">
+        <CompanySidebar />
+      </div>
+      <div class="right-sidebar bg-gray-50">
+        <slot></slot>
+      </div>
+    </div>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+</style>
