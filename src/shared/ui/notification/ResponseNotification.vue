0eb5f713419ebee85e8eb7ddd45720d755808e01
serj2626 <serj2626@mail.ru>
2024-11-19 14:10:34 +0300

    feat: new widget for notification by responses

---
+++ src/shared/ui/notification/ResponseNotification.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <UNotification
+    description="This is a notification."
+    :avatar="{ src: 'https://avatars.githubusercontent.com/u/739984?v=4' }"
+    :id="4"
+    :timeout="0"
+    title="Notification"
+  />
+</template>
+<style scoped lang="scss"></style>
