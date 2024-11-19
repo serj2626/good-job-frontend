ccc31e09dffb9b6ce4b6d409fff34ab8d02cab2e
serj2626 <serj2626@mail.ru>
2024-11-19 14:09:54 +0300

    feat: update height

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,7 @@ import { DefaultSidebar } from "~/src/widgets/default-sidebar";
 import { Footer } from "~/src/widgets/footer";
 </script>
 <template>
-    <div class="grid grid-cols-[minmax(0,300px),1fr] h-screen py-12">
+    <div class="grid grid-cols-[minmax(0,300px),1fr] h-full py-12">
       <div class="left-sidebar">
         <DefaultSidebar />
       </div>
