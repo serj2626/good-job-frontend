175d562f5aae52771a3e9fddef7ed7c7284a2d83
serj2626 <serj2626@mail.ru>
2024-11-20 11:56:16 +0300

    feat: add header block

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,8 +3,13 @@ import { DefaultSidebar } from "~/src/widgets/default-sidebar";
 import { Footer } from "~/src/widgets/footer";
 </script>
 <template>
-  <div class="flex flex-col justify-between h-full">
-   <div class="grid grid-cols-[minmax(0,300px),1fr] h-full py-12">
+  <header class="flex-col items-center inline-flex shadow-2xl bg-slate-300 text-center p-4 rounded-md">
+    <h1 class="text-6xl font-bold py-6">GJ</h1>
+    <p>Good Job - Сайт для программистов и дизайнеров</p>
+  </header>
+
+  <div class="flex flex-col justify-between h-full min-h-screen">
+    <div class="grid grid-cols-[minmax(0,300px),1fr] h-full py-12">
       <div class="left-sidebar">
         <DefaultSidebar />
       </div>
@@ -13,9 +18,6 @@ import { Footer } from "~/src/widgets/footer";
       </div>
     </div>
     <Footer />
-
   </div>
- 
 </template>
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
