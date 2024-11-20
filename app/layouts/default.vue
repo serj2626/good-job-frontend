261033e0d81e4a82b7f4e4fb139e80d4cfbb6cc7
serj2626 <serj2626@mail.ru>
2024-11-20 12:09:30 +0300

    feat: added Header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import { DefaultSidebar } from "~/src/widgets/default-sidebar";
 import { Footer } from "~/src/widgets/footer";
+import { Header } from "~/src/widgets/header";
 </script>
 <template>
-  <header class="flex-col items-center inline-flex shadow-2xl bg-slate-300 text-center p-4 rounded-md">
-    <h1 class="text-6xl font-bold py-6">GJ</h1>
-    <p>Good Job - Сайт для программистов и дизайнеров</p>
-  </header>
+  <Header />
 
   <div class="flex flex-col justify-between h-full min-h-screen">
     <div class="grid grid-cols-[minmax(0,300px),1fr] h-full py-12">
