0c534522eff93ded670b83f53942105a11c19d1d
serj2626 <serj2626@mail.ru>
2024-11-21 12:42:26 +0300

    feat: update list links for footer

--- src/widgets/footer/ui/index.vue
+++ src/widgets/footer/ui/index.vue
@@ -2,9 +2,15 @@
 import { footerLinks } from "../model/footer.data";
 </script>
 <template>
-  <div class="flex justify-center flex-row gap-x-3 h-20 bg-yellow-200 items-center">
-    <div v-for="link in footerLinks" :key="link.title">
-      <a :href="link.url" class="text-[#1C160C]">{{ link.title }}</a>
-    </div>
-  </div>
+  <footer class="flex justify-center gap-3 h-20 items-center bg-slate-50 text-slate-400">
+    <a
+      v-for="link in footerLinks"
+      :key="link.title"
+      :href="link.url"
+      class="text-[#1C160C]"
+      >{{ link.title }}</a
+    >
+  </footer>
 </template>
+<style scoped lang="scss">
+</style>
