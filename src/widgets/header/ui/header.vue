6647725234dbdb56918b5a5e208efce1855b5f20
serj2626 <serj2626@mail.ru>
2024-11-21 12:41:39 +0300

    feat: added style for h1 title

--- src/widgets/header/ui/header.vue
+++ src/widgets/header/ui/header.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts"></script>
 <template>
   <header
-    class="flex-col items-center flex bg-slate-50 text-center p-4 rounded-md"
+    class="flex-col items-center flex  text-center p-2 rounded-lg mb-12"
   >
-    <h1 class="text-6xl font-bold py-6">GJ</h1>
-    <p>Good Job - Сайт для программистов и дизайнеров</p>
+    <h1 class="font-bold my-4 text-slate-600 text-6xl">GJ</h1>
+    <p class="text-slate-500">Good Job - Сайт для программистов и дизайнеров</p>
   </header>
 </template>
 <style scoped lang="scss">
 header {
-  box-shadow: 12px 16px 1px rgba(0, 0, 0, 0.162);
+  box-shadow: 12px 16px 1px rgba(0, 0, 0, 0.190);
+}
+h1{
+  text-shadow: 8px 2px 1px rgba(0, 0, 0, 0.190);
 }
 </style>
