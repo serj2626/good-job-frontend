ffa1a7c0f68a6d2dce97704f4d94a411a1ed906c
serj2626 <serj2626@mail.ru>
2024-11-21 12:44:48 +0300

    feat: added new data

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./app/layouts/**/*.vue",
-    "./src/pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app/index.vue",
-    "./error.vue",
-  ],
+  // content: [
+  //   ".src/pages/**/*.{js,vue,ts}",
+  //   ".src/widgets/**/*.{js,vue,ts}",
+  //   ".src/pages/**/*.{js,vue,ts}",
+  //   ".src/features/**/*.{js,vue,ts}",
+  //   ".src/shared/**/*.{js,vue,ts}",
+  //   ".src/*.{js,vue,ts}",
+  //   "./app/layouts/**/*.vue",
+  //   "./plugins/**/*.{js,ts}",
+  //   "./app/index.vue",
+  //   "./error.vue",
+  // ],
+  content: ['app/**/*.vue', 'layouts/**/*.vue', 'pages/**/*.vue', 'src/**/*.vue'],
   postcss: {
     plugins: {
       tailwindcss: {},
