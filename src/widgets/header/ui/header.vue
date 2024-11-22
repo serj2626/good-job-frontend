dabe4278beee50fa43045fe88281366ded8093be
serj2626 <serj2626@mail.ru>
2024-11-22 12:22:58 +0300

    feat: update style

--- src/widgets/header/ui/header.vue
+++ src/widgets/header/ui/header.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts"></script>
 <template>
-  <header
-    class="flex-col items-center flex  text-center p-2 rounded-lg mb-12"
-  >
+  <header class="flex-col items-center flex text-center p-2 rounded-lg mb-3 shadow-2xl">
     <h1 class="font-bold my-4 text-slate-600 text-6xl">GJ</h1>
     <p class="text-slate-500">Good Job - Сайт для программистов и дизайнеров</p>
   </header>
 </template>
 <style scoped lang="scss">
-header {
-  box-shadow: 12px 16px 1px rgba(0, 0, 0, 0.190);
+// header {
+//   box-shadow: 12px 16px 1px rgba(143, 141, 141, 0.19);
+// }
+h1 {
+  text-shadow: 8px 2px 1px rgba(0, 0, 0, 0.19);
 }
-h1{
-  text-shadow: 8px 2px 1px rgba(0, 0, 0, 0.190);
-}
-p{
-  text-shadow: 0px -2px 2px rgba(0, 0, 0, 0.190);
+
+p {
+ text-shadow: 0px -2px 2px rgba(0, 0, 0, 0.19);
 }
 </style>
