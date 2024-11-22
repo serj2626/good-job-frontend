ea7fbcf5adfd01e1fd81b4f1e563e96bb63b2ead
serj2626 <serj2626@mail.ru>
2024-11-22 12:39:06 +0300

    style: del other tags

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,29 +2,16 @@
 import { DefaultSidebar } from "~/src/widgets/default-sidebar";
 import { Footer } from "~/src/widgets/footer";
 import { Header } from "~/src/widgets/header";
+import { Menu } from "~/src/widgets/menu";
 </script>
 <template>
-  <Header />
-
-  <!-- <div class="flex flex-col justify-between h-full min-h-screen">
-    <div class="grid grid-cols-[minmax(0,300px),1fr] h-full py-12">
-      <div class="left-sidebar">
-        <DefaultSidebar />
-      </div>
-      <div class="right-sidebar">
-        <slot></slot>
-      </div>
-    </div>
-    <Footer />
-  </div> -->
   <div class="flex flex-col justify-between h-full min-h-screen">
-    <div class=" h-full py-12">
-        <DefaultSidebar />
-      <div class="">
-        <slot></slot>
-      </div>
+    <Header />
+    <div class="h-full pb-12">
+      <DefaultSidebar />
+      <Menu />
+      <slot></slot>
     </div>
     <Footer />
   </div>
 </template>
-<style scoped lang="scss"></style>
