1561e515269819587d23b903db1a9cbc80167381
serj2626 <serj2626@mail.ru>
2024-11-23 14:47:40 +0300

    feat: del snippet Menu

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,14 +2,12 @@
 import { DefaultSidebar } from "~/src/widgets/default-sidebar";
 import { Footer } from "~/src/widgets/footer";
 import { Header } from "~/src/widgets/header";
-import { Menu } from "~/src/widgets/menu";
 </script>
 <template>
   <div class="flex flex-col justify-between h-full min-h-screen">
     <Header />
     <div class="h-full pb-12">
       <DefaultSidebar />
-      <Menu />
       <slot></slot>
     </div>
     <Footer />
