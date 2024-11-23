6f302c495b5ac36bbac667a92a06b6ed8a2c1299
serj2626 <serj2626@mail.ru>
2024-11-23 14:48:02 +0300

    feat: new page for error

---
+++ app/error.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps<{ error: NuxtError }>();
+</script>
+<template>
+  <h1>{{ error.message }}</h1>
+  <pre>{{ error }}</pre>
+</template>
+<style scoped lang="scss"></style>
