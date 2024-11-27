1844d754d8e4dc892912906ba991045ccb3a1808
serj2626 <serj2626@mail.ru>
2024-11-27 22:50:01 +0300

    feat: new types for company

---
+++ src/pages/company-detail/model/company.type.ts
@@ -0,0 +1,21 @@
+interface IUser {
+  email: string;
+  type: string;
+}
+
+interface ICompany {
+  user: IUser;
+  phone: string;
+  country: string;
+  city: string;
+  name: string;
+  site: string;
+  about: string;
+  slug: string;
+  created_at: string;
+  updated_at: string;
+  avatar: string;
+  count_employees: number;
+}
+
+export type { IUser, ICompany };
