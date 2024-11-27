2641e351bc5bf648a32ac7d641e71e909a3dad68
serj2626 <serj2626@mail.ru>
2024-11-27 22:45:08 +0300

    feat: add fale data

---
+++ src/pages/vacancy-detail/ui/vacancy-detail-page.vue
@@ -0,0 +1,151 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="px-3 flex flex-1 justify-center pb-5 shadow-sm">
+    <div class="flex flex-col max-w-[960px] flex-1">
+      <div class="flex flex-wrap justify-between gap-3 p-4">
+        <p
+          class="text-[#111418] tracking-light text-[32px] font-bold leading-tight min-w-72"
+        >
+          Senior Software Engineer
+        </p>
+      </div>
+      <div
+        class="flex items-center gap-4 bg-white px-4 min-h-[72px] py-2 justify-between"
+      >
+        <div class="flex items-center gap-4">
+          <div
+            class="text-[#111418] flex items-center justify-center rounded-lg bg-[#f0f2f4] shrink-0 size-12"
+            data-icon="Buildings"
+            data-size="24px"
+            data-weight="regular"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+            >
+              <path
+                d="M240,208H224V96a16,16,0,0,0-16-16H144V32a16,16,0,0,0-24.88-13.32L39.12,72A16,16,0,0,0,32,85.34V208H16a8,8,0,0,0,0,16H240a8,8,0,0,0,0-16ZM208,96V208H144V96ZM48,85.34,128,32V208H48ZM112,112v16a8,8,0,0,1-16,0V112a8,8,0,1,1,16,0Zm-32,0v16a8,8,0,0,1-16,0V112a8,8,0,1,1,16,0Zm0,56v16a8,8,0,0,1-16,0V168a8,8,0,0,1,16,0Zm32,0v16a8,8,0,0,1-16,0V168a8,8,0,0,1,16,0Z"
+              ></path>
+            </svg>
+          </div>
+          <div class="flex flex-col justify-center">
+            <p
+              class="text-[#111418] text-base font-medium leading-normal line-clamp-1"
+            >
+              Microsoft
+            </p>
+            <p
+              class="text-[#637588] text-sm font-normal leading-normal line-clamp-2"
+            >
+              Удалённо
+            </p>
+          </div>
+        </div>
+        <div class="shrink-0">
+          <p class="text-[#111418] text-base font-normal leading-normal">
+            40000 - 60000 &#8381
+          </p>
+        </div>
+      </div>
+      <h3
+        class="text-[#111418] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4"
+      >
+        Описание
+      </h3>
+      <p
+        class="text-[#111418] text-base font-normal leading-normal pb-3 pt-1 px-4"
+      >
+      Мы ищем старшего инженера-программиста для разработки и внедрения функциональных программных решений. Вы будете работать с высшим руководством над определением требований к программному обеспечению и возглавлять операционные и технические проекты. На этой должности вы должны уметь работать независимо, без особого контроля. Вы должны обладать отличными организаторскими способностями и навыками решения проблем. Если у вас также есть практический опыт разработки программного обеспечения и гибких методологий, мы будем рады с вами познакомиться. Вашей целью будет разработка высококачественного программного обеспечения, соответствующего потребностям пользователей и бизнес-целям.
+      </p>
+      <h3
+        class="text-[#111418] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4"
+      >
+        Необходимые навыки
+      </h3>
+      <div class="flex gap-3 p-3 flex-wrap pr-4">
+        <div
+          class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#f0f2f4] pl-4 pr-4"
+        >
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Python
+          </p>
+        </div>
+        <div
+          class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#f0f2f4] pl-4 pr-4"
+        >
+          <p class="text-[#111418] text-sm font-medium leading-normal">Java</p>
+        </div>
+        <div
+          class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#f0f2f4] pl-4 pr-4"
+        >
+          <p class="text-[#111418] text-sm font-medium leading-normal">C#</p>
+        </div>
+        <div
+          class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#f0f2f4] pl-4 pr-4"
+        >
+          <p class="text-[#111418] text-sm font-medium leading-normal">Agile</p>
+        </div>
+        <div
+          class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#f0f2f4] pl-4 pr-4"
+        >
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Software Development
+          </p>
+        </div>
+      </div>
+      <h3
+        class="text-[#111418] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4"
+      >
+        Benefits
+      </h3>
+      <div class="flex gap-3 p-3 flex-wrap pr-4">
+        <div
+          class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#f0f2f4] pl-4 pr-4"
+        >
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Health Insurance
+          </p>
+        </div>
+        <div
+          class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#f0f2f4] pl-4 pr-4"
+        >
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Retirement Plan
+          </p>
+        </div>
+        <div
+          class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#f0f2f4] pl-4 pr-4"
+        >
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Flexible Hours
+          </p>
+        </div>
+        <div
+          class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#f0f2f4] pl-4 pr-4"
+        >
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Paid Time Off
+          </p>
+        </div>
+        <div
+          class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#f0f2f4] pl-4 pr-4"
+        >
+          <p class="text-[#111418] text-sm font-medium leading-normal">
+            Remote Work
+          </p>
+        </div>
+      </div>
+      <div class="flex px-4 py-3">
+        <button
+          class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-12 px-5 flex-1 bg-[#1980e6] text-white text-base font-bold leading-normal tracking-[0.015em]"
+        >
+          <span class="truncate">Откликнуться</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss"></style>
