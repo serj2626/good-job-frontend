c9cc59f7fb3abef11f66a39004fef7b2ae6ee920
serj2626 <serj2626@mail.ru>
2024-11-27 13:43:48 +0300

    feat: repalce image

--- src/widgets/footer/ui/index.vue
+++ src/widgets/footer/ui/index.vue
@@ -20,9 +20,11 @@ import { footerLinks } from "../model/footer.data";
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" class="flex items-center">
-                  <img src="/image/title.png" class="h-20 shadow-2xl me-3" alt="FlowBite Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Good Job</span>
+              <a href="" class="flex flex-col gap-4 items-center">
+                  <img src="/image/title.jpg" class="h-20 shadow-2xl me-3" alt="Good Job" />
+                  <span class="self-center text-lg font-serif whitespace-nowrap dark:text-white">
+                    Good Job
+                </span>
               </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -64,7 +66,7 @@ import { footerLinks } from "../model/footer.data";
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <section id="footer-links">
               <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">Good Job</a>. Все права защищены.
           </span>
           <div class="flex mt-4 sm:justify-center sm:mt-0">
               <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
