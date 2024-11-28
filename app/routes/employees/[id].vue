9c8886fd198552aaf287632da369dc017970fe6c
serj2626 <serj2626@mail.ru>
2024-11-29 00:48:31 +0300

    feat: add snippet EmployeeDetailPage

---
+++ app/routes/employees/[id].vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { EmployeeDetailPage } from "~/src/pages/employee-detail";
+</script>
+<template>
+  <EmployeeDetailPage />
+</template>
+<style scoped lang="scss"></style>
