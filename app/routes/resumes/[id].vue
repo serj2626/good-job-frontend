1607e283722861846e5ed958c744fcb3a71c56e5
serj2626 <serj2626@mail.ru>
2024-11-29 00:47:43 +0300

    feat: add import snippet resume detail

---
+++ app/routes/resumes/[id].vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { ResumeDetailPage } from "~/src/pages/resume-detail";
+</script>
+<template>
+  <ResumeDetailPage />
+</template>
+<style scoped lang="scss"></style>
