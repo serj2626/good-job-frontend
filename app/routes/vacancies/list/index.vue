09954df75385d0f064dd4d5fbf059b694d7e37fe
serj2626 <serj2626@mail.ru>
2024-11-29 00:45:41 +0300

    feat: add import VacancyListPage

---
+++ app/routes/vacancies/list/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { VacancyListPage } from '~/src/pages/vacancy-list';
+</script>
+<template>
+    <VacancyListPage />
+</template>
+<style scoped lang="scss"></style>
