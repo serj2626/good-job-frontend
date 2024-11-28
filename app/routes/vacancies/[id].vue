66034b20d2a8444ed89a9d2f81fa4c6581518737
serj2626 <serj2626@mail.ru>
2024-11-29 00:46:16 +0300

    feat: add code

---
+++ app/routes/vacancies/[id].vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { VacancyDetailPage } from "~/src/pages/vacancy-detail";
+</script>
+<template>
+  <VacancyDetailPage />
+</template>
+<style scoped lang="scss"></style>
