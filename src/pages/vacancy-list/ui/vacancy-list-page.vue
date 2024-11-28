19f8981a9514e52df674c9f05dd89db124cfed02
serj2626 <serj2626@mail.ru>
2024-11-28 21:43:32 +0300

    feat: add func for get all vacancies

--- src/pages/vacancy-list/ui/vacancy-list-page.vue
+++ src/pages/vacancy-list/ui/vacancy-list-page.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
-const options = [{
-  value: 'full',
-  label: 'Полный день'
-}, {
-  value: 'part',
-  label: 'Частичная занятость'
-}, {
-  value: 'remote',
-  label: 'Удаленная работа'
-}]
-const categories = ['Разработка', 'Дизайн', 'Бизнес', 'Продажи', 'Маркетинг', 'Другое']
-const selected = ref('sms')
-const salaries = [
+const config = useRuntimeConfig();
+const {
+  data: vacancies,
+  pending,
+  error,
+  refresh,
+} = await useAsyncData("vacancies", () =>
+  $fetch(config.public.apiUrl + `/api/companies/vacancy/`)
+);
 
+const options = [
+  {
+    value: "full",
+    label: "Полный день",
+  },
+  {
+    value: "part",
+    label: "Частичная занятость",
+  },
+  {
+    value: "remote",
+    label: "Удаленная работа",
+  },
+];
+const categories = [
+  "Разработка",
+  "Дизайн",
+  "Бизнес",
+  "Продажи",
+  "Маркетинг",
+  "Другое",
+];
+const selected = ref("sms");
+const salaries = [
   "Не имеет значения",
   "От 10 000 ₽",
   "от 25 000 ₽",
@@ -20,61 +40,119 @@ const salaries = [
   "от 100 000 ₽",
   "от 140 000 ₽",
   "от 180 000 ₽",
-  "от 220 000 ₽"
-]
+  "от 220 000 ₽",
+];
 const experience = [
   "Не имеет значения ",
   "Нет опыта ",
   "От 1 года до 3 лет ",
   "От 3 до 6 лет ",
-  "Более 6 лет"
-]
+  "Более 6 лет",
+];
 </script>
 
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-[minmax(0,300px),1fr] gap-2">
-    <div class="left-block flex flex-col gap-8"">
-    <section id=" search" class="bg-gray-100 text-center shadow-md p-3 flex flex-col gap-2">
-      <h3 class="text-[#111418] text-lg font-bold px-4 pb-2 pt-4 mb-4">
-        Поиск вакансий
-      </h3>
-      <UInput color="white" variant="outline" placeholder="Введите название должности..." />
-      <USelect color="white" variant="outline" :options="['United States', 'Canada', 'Mexico']" />
-      <USelect color="white" variant="outline" :options="['Санкт-Петербург', 'Москва', 'Самара', 'Казань']" />
+    <div class="left-block flex flex-col gap-8">
+      <section
+        id=" search"
+        class="bg-gray-100 text-center shadow-md p-3 flex flex-col gap-2"
+      >
+        <h3 class="text-[#111418] text-lg font-bold px-4 pb-2 pt-4 mb-4">
+          Поиск вакансий
+        </h3>
+        <UInput
+          color="white"
+          variant="outline"
+          placeholder="Введите название должности..."
+        />
+        <USelect
+          color="white"
+          variant="outline"
+          :options="['United States', 'Canada', 'Mexico']"
+        />
+        <USelect
+          color="white"
+          variant="outline"
+          :options="['Санкт-Петербург', 'Москва', 'Самара', 'Казань']"
+        />
 
-      <div class="flex flex-col gap-5 my-4">
-        <URadioGroup v-model="selected" legend="График работы" :options="options" />
-        <URadioGroup v-model="selected" legend="Специализации" :options="categories" />
-        <URadioGroup v-model="selected" legend="Уровень дохода" :options="salaries" />
-        <URadioGroup v-model="selected" legend="Требуемый опыт работы" :options="experience" />
-      </div>
-      <UButton color="teal" variant="outline" size="lg">Найти</UButton>
+        <div class="flex flex-col gap-5 my-4">
+          <URadioGroup
+            v-model="selected"
+            legend="График работы"
+            :options="options"
+          />
+          <URadioGroup
+            v-model="selected"
+            legend="Специализации"
+            :options="categories"
+          />
+          <URadioGroup
+            v-model="selected"
+            legend="Уровень дохода"
+            :options="salaries"
+          />
+          <URadioGroup
+            v-model="selected"
+            legend="Требуемый опыт работы"
+            :options="experience"
+          />
+        </div>
+        <UButton color="teal" variant="outline" size="lg">Найти</UButton>
       </section>
       <section id="sorting"></section>
     </div>
     <div class="right-block px-5 flex flex-col gap-8 shadow-md bg-gray-100">
-      <h3 class="text-[#111418] text-lg font-bold px-4 pb-2 pt-4 mb-4">Вакансии</h3>
-      <UAccordion color="teal" variant="solid" size="xl" open-icon="i-heroicons-plus" close-icon="i-heroicons-minus"
-        :items="[
-          {
-            label: '1. Frontend Development / Saint-Petersburg',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-          },
-          {
-            label: '2. Backend Development / Moscow',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-          },
-          {
-            label: '3. Fullstack Development / Samara',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-          },
-          {
-            label: '4. Testing / Kazan',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-          },
-        ]" />
-                <UPagination size="sm" :model-value="1" :total="100" show-last show-first class="flex justify-center my-2">
-                </UPagination>
+      <h3 class="text-[#111418] text-lg font-bold px-4 pb-2 pt-4 mb-4">
+        Вакансии
+      </h3>
+      <UAccordion :items="vacancies">
+        <template #item="{ item }">
+          <p class="italic text-gray-900 dark:text-white text-center">
+            {{ item.position }} - {{ item.category }}
+          </p>
+        </template>
+
+        <template #getting-started>
+      <div class="text-gray-900 dark:text-white text-center">
+        <Logo class="w-auto h-8 mx-auto" />
+
+        <p class="text-sm text-gray-500 dark:text-gray-400 mt-2">
+          Fully styled and customizable components for Nuxt.
+        </p>
+      </div>
+    </template>
+
+        <template #installation="{ description }">
+          <div class="flex flex-col justify-center items-center gap-1 mb-4">
+            <h3 class="text-xl font-bold text-gray-900 dark:text-white">
+              Installation
+            </h3>
+            <p class="text-sm text-gray-500 dark:text-gray-400">
+              Install <code>@nuxt/ui</code> dependency to your project:
+            </p>
+            <p>
+           s
+            </p>
+          </div>
+
+          <div class="flex flex-col items-center">
+            <code>$ npx nuxi@latest module add ui</code>
+          </div>
+        </template>
+      </UAccordion>
+
+
+      <UPagination
+        size="sm"
+        :model-value="1"
+        :total="100"
+        show-last
+        show-first
+        class="flex justify-center my-2"
+      >
+      </UPagination>
     </div>
   </div>
 </template>
