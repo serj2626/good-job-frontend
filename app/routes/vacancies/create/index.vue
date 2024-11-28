c7213a59b390cc7197fd65f5cedebaff7b5d68a5
serj2626 <serj2626@mail.ru>
2024-11-29 00:45:53 +0300

    feat: add import VacancyCreatePage

---
+++ app/routes/vacancies/create/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { VacancyCreatePage } from '~/src/pages/vacancy-create';
+</script>
+<template>
+  <VacancyCreatePage />
+</template>
+<style scoped lang="scss"></style>
