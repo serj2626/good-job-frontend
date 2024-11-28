1f4b7a240c86f66543c9d85d8ea006149729bcfd
serj2626 <serj2626@mail.ru>
2024-11-29 00:48:56 +0300

    feat: add snippet CompanyDetailPage

---
+++ app/routes/companies/[id].vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { CompanyDetailPage } from '~/src/pages/company-detail';
+</script>
+<template>
+    <CompanyDetailPage />
+</template>
+<style scoped lang="scss"></style>
