6a988d2e1699e54d2fe010ca539737477d82b366
serj2626 <serj2626@mail.ru>
2024-11-29 13:53:07 +0300

    feat: new type vacancy

---
+++ src/pages/vacancy-list/model/vacancy.type.ts
@@ -0,0 +1,23 @@
+import type { ICompany } from "../../company-detail/model/company.type";
+
+export interface IVacancy {
+  id: number;
+  time_ago: string;
+  company: ICompany;
+  stacks: string[];
+  category: string;
+  position: string;
+  min_salary: number;
+  max_salary: number;
+  work_schedule: string;
+  created_at: string;
+  updated_at: string;
+  level: string;
+  status_vacancy: string;
+  work_experience: number;
+  requirements: string;
+  description: string;
+  city: string;
+  country: string;
+  metro?: string;
+}
