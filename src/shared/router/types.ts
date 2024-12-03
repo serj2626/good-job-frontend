1902d85567f243fb204dac05cd4568ab1a93ffb4
serj2626 <serj2626@mail.ru>
2024-12-03 14:53:33 +0300

    feat: new router types

---
+++ src/shared/router/types.ts
@@ -0,0 +1,33 @@
+import type { RouteLocationNamedRaw } from "vue-router";
+
+export const enum AppRoutes {
+  index = "index",
+
+  authLogin = "auth-login",
+  authRegistration = "auth-registration",
+  authLogout = "auth-logout",
+  authReset = "auth-reset",
+
+  companiesId = "companies-id",
+  companiesList = "companies-list",
+
+  employeesId = "employees-id",
+  employeesList = "employees-list",
+
+  resumesId = "resumes-id",
+  resumesList = "resumes-list",
+  resumesCreate = "resumes-create",
+
+  vacanciesList = "vacancies-list",
+  vacanciesId = "vacancies-id",
+  vacanciesCreate = "vacancies-create",
+
+  interviews = "interviews",
+
+  projectsId = "projects-id",
+  projectsCreate = "projects-create",
+}
+
+export interface AppRoute extends RouteLocationNamedRaw {
+  name: AppRoutes;
+}
