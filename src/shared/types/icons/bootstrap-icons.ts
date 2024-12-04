d027b8e0c041b66380706fe760ca8fd0449f8f89
serj2626 <serj2626@mail.ru>
2024-12-04 13:42:06 +0300

    feat: new icons

---
+++ src/shared/types/icons/bootstrap-icons.ts
@@ -0,0 +1,11 @@
+export const enum BootstrapIcons {
+  CHECK_COMPANY = "bi:check-circle-fill",
+  X_CHECK_COMPANY = "bi:x-circle-fill",
+  GITHUB = "bi:github",
+  GITLAB = "bi:gitlab",
+  LINKEDIN = "bi:linkedin",
+  MAIL = "bi:envelope-at-fill",
+  PHONE = "bi:telephone-fill",
+  TELEGRAM = "bi:telegram",
+  POSITION = "bi:person-workspace", 
+}
