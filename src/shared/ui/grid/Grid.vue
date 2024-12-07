6307aaf72d7e2467b28972e58177aeda83b8b174
serj2626 <serj2626@mail.ru>
2024-12-07 18:36:32 +0300

    feat: new component

---
+++ src/shared/ui/grid/Grid.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-[minmax(0,300px),1fr] gap-3">
+    <div class="left-block flex flex-col gap-6">
+      <slot name="left"></slot>
+    </div>
+    <main
+      id="right"
+      class="right-block flex flex-col gap-6 shadow-md bg-gray-100 p-3"
+    >
+      <slot name="right"></slot>
+    </main>
+  </div>
+</template>
