f252bd5fa323cece5ea25590836833c1de4c3d98
serj2626 <serj2626@mail.ru>
2024-12-07 17:20:17 +0300

    feat: new file menu

---
+++ src/features/menu/ui/Menu.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { MenuLinks } from "../model/menu.data";
+</script>
+
+<template>
+  <menu
+    class="flex justify-center flex-wrap items-center gap-3 sm:gap-10 py-3 mt-2"
+  >
+    <NuxtLink
+      v-for="link in MenuLinks"
+      :key="link.title"
+      :to="link.url"
+      class="menu-link py-2"
+    >
+      {{ link.title }}
+    </NuxtLink>
+  </menu>
+</template>
+
+<style lang="scss" scoped></style>
