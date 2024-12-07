6c97b80bc63c6e3dcb9b65fdd2ef278dd267fa6a
serj2626 <serj2626@mail.ru>
2024-12-07 17:20:07 +0300

    feat: new data for menu

---
+++ src/features/menu/model/menu.data.ts
@@ -0,0 +1,31 @@
+interface IMenuItem {
+  title: string;
+  url: string;
+}
+
+export const MenuLinks: IMenuItem[] = [
+  {
+    title: "Главная",
+    url: "/",
+  },
+  {
+    title: "Вакансии",
+    url: "/vacancies/list",
+  },
+  {
+    title: "Компании",
+    url: "/companies/list",
+  },
+  {
+    title: "Специалисты",
+    url: "/employees/list",
+  },
+  {
+    title: "Резюме",
+    url: "/resumes/list",
+  },
+  {
+    title: "Посты и события",
+    url: "/",
+  },
+];
