dddc3fd24c81d48d49d946c48f53b3a05a49dc8f
serj2626 <serj2626@mail.ru>
2024-12-10 00:14:47 +0300

    feat: updated color by btn in dark mode

--- src/pages/home/ui/home-page.vue
+++ src/pages/home/ui/home-page.vue
@@ -8,11 +8,11 @@ const { data: categories } = await useFetch(
   <div class="flex flex-col justify-center items-center gap-8">
     <div class="flex min-w-72 flex-col gap-3 items-center">
       <p
-        class="text-[#111418] text-[32px] font-bold bg-gradient-to-tr from-[#787a7c] to-[#38414d] bg-clip-text text-transparent leading-tight tracking-[-0.015em]"
+        class="text-[#111418] dark:text-white text-[32px] font-bold bg-gradient-to-tr from-[#787a7c] to-[#38414d] bg-clip-text text-transparent leading-tight tracking-[-0.015em]"
       >
         Найди свою работу мечты
       </p>
-      <p class="text-[#637588] text-sm font-normal leading-normal">
+      <p class="text-[#637588] dark:text-white text-sm font-normal leading-normal">
         Просмотри сотни тысяч объявлений о вакансиях, чтобы найти идеальную
         работу для себя
       </p>
@@ -23,6 +23,7 @@ const { data: categories } = await useFetch(
       size="xl"
       color="teal"
       variant="solid"
+      class="dark:bg-teal-400 dark:hover:bg-teal-500"
     >
       <template #trailing>
         <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
