a7d3ccfec63e1f2fc1e3d28aeaafec67da340957
serj2626 <serj2626@mail.ru>
2024-12-10 01:03:05 +0300

    feat: new file

---
+++ src/shared/ui/typography/Typography.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+interface Props {
+  tag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span" | "div";
+  class: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <component :class="props.class" :is="props.tag">
+    <slot />
+  </component>
+</template>
