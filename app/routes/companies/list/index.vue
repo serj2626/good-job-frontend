a7ce8c4f17980050c494ec6e41f4e4fe0c300b0c
serj2626 <serj2626@mail.ru>
2024-12-10 00:09:52 +0300

    style: formatting code

--- app/routes/companies/list/index.vue
+++ app/routes/companies/list/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { CompanyListPage } from "~/src/pages/company-list";
+import { CompanyListPage } from '~/src/pages/company-list';
 </script>
 <template>
   <CompanyListPage />
