46e7150694dc7025cb6ccaf2e4df2badf96b6288
serj2626 <serj2626@mail.ru>
2024-12-10 00:13:27 +0300

    style: del other code and spaces

--- src/pages/company-list/ui/company-list-page.vue
+++ src/pages/company-list/ui/company-list-page.vue
@@ -1,72 +1,21 @@
 <script setup lang="ts">
-import type { ICompany } from "../../company-detail/model/company.type";
-import CompanyDetail from "./CompanyDetail.vue";
-import Grid from "~/src/shared/ui/grid/Grid.vue";
+import { Search } from '~/src/features/search';
+import Grid from '~/src/shared/ui/grid/Grid.vue';
+
+import type { ICompany } from '../../company-detail/model/company.type';
+import CompanyDetail from './CompanyDetail.vue';
+
 const config = useRuntimeConfig();
 const { data: companies } = await useLazyAsyncData<ICompany[]>(
   "companies",
   async () => $fetch(config.public.apiUrl + `/api/companies/`)
 );
-const hasVacancy = ["Не имеет значения", "Да", "Нет"];
-const selected = ref("sms");
-const isVerified = ["Не имеет значения", "Да", "Нет"];
-const countEmployees = [
-  "Не имеет значения ",
-  " до 10 человек",
-  " от 10 человек",
-  " от 20 человек",
-  " от 30 человек",
-  " более 50 человек",
-];
 </script>
 
 <template>
   <Grid>
     <template #left>
-      <div>
-        <section
-          id="search"
-          class="bg-gray-100 text-center shadow-md px-3 pb-4 flex flex-col gap-2"
-        >
-          <h3 class="text-[#111418] text-lg font-bold px-4 pb-2 mb-4">
-            Поиск компаний
-          </h3>
-          <UInput
-            color="white"
-            variant="outline"
-            placeholder="Введите название компании..."
-          />
-          <USelect
-            color="white"
-            variant="outline"
-            :options="['Россия', 'Беларусь', 'Казахстан']"
-          />
-          <USelect
-            color="white"
-            variant="outline"
-            :options="['Санкт-Петербург', 'Москва', 'Самара', 'Казань']"
-          />
-
-          <div class="flex flex-col gap-5 my-4">
-            <URadioGroup
-              v-model="selected"
-              legend="Наличие вакансий"
-              :options="hasVacancy"
-            />
-            <URadioGroup
-              v-model="selected"
-              legend="Проверенная компания"
-              :options="isVerified"
-            />
-            <URadioGroup
-              v-model="selected"
-              legend="Количество сотрудников"
-              :options="countEmployees"
-            />
-          </div>
-          <UButton color="teal" variant="outline" size="lg">Найти</UButton>
-        </section>
-      </div>
+      <Search />
     </template>
     <template #right>
       <div>
