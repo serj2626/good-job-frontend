a33c10f4ceac988d00631a7ec0f1217790392004
serj2626 <serj2626@mail.ru>
2024-12-10 00:14:23 +0300

    style: del other code

--- src/pages/employee-list/ui/employess-list-page.vue
+++ src/pages/employee-list/ui/employess-list-page.vue
@@ -1,77 +1,28 @@
 <script setup lang="ts">
-import Grid from "~/src/shared/ui/grid/Grid.vue";
-import type { IEmployee } from "../../employee-detail/model/employee.type";
-import EmployeeDetail from "./EmployeeDetail.vue";
+import { Search } from '~/src/features/search';
+import Grid from '~/src/shared/ui/grid/Grid.vue';
+
+import type { IEmployee } from '../../employee-detail/model/employee.type';
+import EmployeeDetail from './EmployeeDetail.vue';
 
 const config = useRuntimeConfig();
 const { data: employees } = await useAsyncData<IEmployee[]>("employees", () =>
   $fetch(config.public.apiUrl + `/api/employees/`)
 );
-const hasVacancy = ["Не имеет значения", "Да", "Нет"];
-const selected = ref("sms");
-const isVerified = ["Не имеет значения", "Да", "Нет"];
-const countEmployees = [
-  "Не имеет значения ",
-  " до 10 человек",
-  " от 10 человек",
-  " от 20 человек",
-  " от 30 человек",
-  " более 50 человек",
-];
 </script>
 <template>
   <Grid>
     <template #left>
-      <div>
-        <section
-          id="search"
-          class="bg-gray-100 text-center shadow-md px-3 pb-4 flex flex-col gap-2"
-        >
-          <h3 class="text-[#111418] text-lg font-bold px-4 pb-2 mb-4">
-            Поиск специалистов
-          </h3>
-          <UInput
-            color="white"
-            variant="outline"
-            placeholder="Введите название компании..."
-          />
-          <USelect
-            color="white"
-            variant="outline"
-            :options="['Россия', 'Беларусь', 'Казахстан']"
-          />
-          <USelect
-            color="white"
-            variant="outline"
-            :options="['Санкт-Петербург', 'Москва', 'Самара', 'Казань']"
-          />
-
-          <div class="flex flex-col gap-5 my-4">
-            <URadioGroup
-              v-model="selected"
-              legend="Наличие вакансий"
-              :options="hasVacancy"
-            />
-            <URadioGroup
-              v-model="selected"
-              legend="Проверенная компания"
-              :options="isVerified"
-            />
-            <URadioGroup
-              v-model="selected"
-              legend="Количество сотрудников"
-              :options="countEmployees"
-            />
-          </div>
-          <UButton color="teal" variant="outline" size="lg">Найти</UButton>
-        </section>
-      </div>
+      <Search />
     </template>
     <template #right>
       <div>
-        <h3 class="text-[#111418] text-lg font-bold px-4 pb-2 mb-4">
+        <Typography
+          class="text-[#111418] text-lg font-bold px-4 pb-2 mb-4"
+          tag="h3"
+        >
           Специалисты
-        </h3>
+        </Typography>
         <EmployeeDetail
           v-for="(employee, index) in employees"
           :key="index"
