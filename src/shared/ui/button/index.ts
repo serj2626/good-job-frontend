e19911de0efb9f20e900d5cd59a0a751df81a65d
serj2626 <serj2626@mail.ru>
2024-12-10 01:53:16 +0300

    feat: created global api for btns

---
+++ src/shared/ui/button/index.ts
@@ -0,0 +1,7 @@
+import ArrowButton from "./ArrowButton.vue";
+import EditButton from "./EditButton.vue";
+import XButton from "./XButton.vue";
+import DropDownButton from "./DropDownButton.vue";
+import BurgerButton from "./BurgerButton.vue";
+
+export { ArrowButton, EditButton, XButton, DropDownButton, BurgerButton };
