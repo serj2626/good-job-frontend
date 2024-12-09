09d53345ada55be0beb7a7330e30fb39c11e6b4e
serj2626 <serj2626@mail.ru>
2024-12-10 01:53:43 +0300

    feat: added UserProfile instead of text

--- src/widgets/company/ui/Company.vue
+++ src/widgets/company/ui/Company.vue
@@ -1,69 +1,13 @@
 <script lang="ts" setup>
-import type { ICompany } from '~/src/pages/company-detail/model/company.type';
-import { BootstrapIcons } from '~/src/shared/types/icons/bootstrap-icons';
+import { UserProfile } from "~/src/features/user-profile";
+import type { ICompany } from "~/src/pages/company-detail/model/company.type";
 
 defineProps<{ company: ICompany }>();
 </script>
 
 <template>
-  <section id="profile" class="relative bg-gray-100 text-center shadow-md p-3">
-    <div class="flex justify-between items-center">
-      <UTooltip
-        v-if="company?.is_verified"
-        text="Компания предоставила все документы"
-        :popper="{ placement: 'top' }"
-      >
-        <span class="text-green-500"
-          >Проверена
-          <Icon
-            :name="BootstrapIcons.CHECK_COMPANY"
-            class="w-6 h-6 text-green-500"
-          />
-        </span>
-      </UTooltip>
-      <UTooltip
-        v-else
-        text="Компания не предоставила документы"
-        :popper="{ placement: 'top-end' }"
-      >
-        <span class="text-red-600">Не проверена</span>
-      </UTooltip>
+  <UserProfile :company="company" />
 
-      <UButton
-        v-if="company?.user.online"
-        color="green"
-        size="md"
-        variant="solid"
-        label="В сети"
-        disabled
-      />
-      <UButton
-        v-else
-        color="red"
-        size="md"
-        variant="solid"
-        label="Не в сети"
-        disabled
-      />
-    </div>
-
-    <img
-      :src="company?.avatar"
-      :alt="company?.name"
-      class="rounded-xl mb-2 mt-3"
-    />
-    <h3 class="text-[#111418] text-2xl font-bold px-4 pb-2 pt-4 mb-3">
-      {{ company?.full_name }}
-    </h3>
-    <div
-      class="text-[#111418] text-base font-normal mb-4 flex justify-center items-center gap-2"
-    >
-      {{ company?.user.type }}
-    </div>
-    <UButton size="xl" loading color="teal" variant="solid" class="w-full flex justify-center">
-      Подружиться
-    </UButton>
-  </section>
   <section id="info" class="bg-gray-100 shadow-md flex flex-col gap-3 p-3">
     <div class="flex justify-between items-center">
       <span class="bg-slate-200 rounded-2xl px-3 py-2">Страна:</span>
