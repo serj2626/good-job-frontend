9b861b00ee0f267b855d853a422976d1fdba15a5
serj2626 <serj2626@mail.ru>
2024-12-10 01:02:52 +0300

    feat:  add style by dark mode

--- src/shared/ui/grid/Grid.vue
+++ src/shared/ui/grid/Grid.vue
@@ -5,7 +5,7 @@
     </div>
     <main
       id="right"
-      class="right-block flex flex-col gap-6 shadow-md bg-gray-100 p-3"
+      class="right-block flex flex-col gap-6 shadow-md bg-gray-100 p-3 pb-8 dark:bg-white"
     >
       <slot name="right"></slot>
     </main>
