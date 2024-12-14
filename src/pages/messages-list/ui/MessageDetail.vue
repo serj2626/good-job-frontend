a5f1b330a9f3f5c648365c8f24d3d4c83d5fd96d
serj2626 <serj2626@mail.ru>
2024-12-14 16:45:31 +0300

    feat: new component for msg

---
+++ src/pages/messages-list/ui/MessageDetail.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="px-3 flex items-center bg-grey-light cursor-pointer">
+    <div>
+      <img
+        class="h-12 w-12 rounded-full"
+        src="https://darrenjameseeley.files.wordpress.com/2014/09/expendables3.jpeg"
+      />
+    </div>
+    <div class="ml-4 flex-1 border-b border-grey-lighter py-4">
+      <div class="flex items-bottom justify-between">
+        <p class="text-grey-darkest">New Movie! Expendables 4</p>
+        <p class="text-xs text-grey-darkest">12:45 pm</p>
+      </div>
+      <p class="text-grey-dark mt-1 text-sm">Get Andrés on this movie ASAP!</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
