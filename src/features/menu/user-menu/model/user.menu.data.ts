36b8283584bf407c5e4206b8fc2affb013497415
serj2626 <serj2626@mail.ru>
2024-12-14 16:43:19 +0300

    feat: new links for user

---
+++ src/features/menu/user-menu/model/user.menu.data.ts
@@ -0,0 +1,56 @@
+import { HeroIcons } from "~/src/shared/types/icons/hero-icons";
+
+interface IMenuItem {
+  label: string;
+  icon?: string;
+  slot?: string;
+  disabled?: boolean;
+}
+
+export const userMenuLins: IMenuItem[][] = [
+  [
+    {
+      label: "serj2626@mail.ru",
+      slot: "account",
+      disabled: true,
+    },
+  ],
+  [
+    {
+      label: "Профиль",
+      icon: HeroIcons.USER,
+    },
+    {
+      label: "Настройки",
+      icon: HeroIcons.SETTINGS,
+    },
+  ],
+  [
+    {
+      label: "Избранное",
+      icon: HeroIcons.HEART,
+    },
+    {
+      label: "Отклики",
+      icon: HeroIcons.RESPONSES,
+    },
+    {
+      label: "Сообщения",
+      icon: HeroIcons.MAIL,
+    },
+    {
+      label: "Обратная связь",
+      icon: HeroIcons.CHAT_BUBBLE_LEFT_RIGHT,
+    },
+  ],
+  [
+    {
+      label: "Войти",
+      icon: HeroIcons.LOGIN,
+    },
+    {
+      label: "Выход",
+      icon: HeroIcons.LOGOUT,
+    },
+  ],
+];
