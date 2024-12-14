63e6023717085f86f6203b85cfc4d989436546af
serj2626 <serj2626@mail.ru>
2024-12-14 16:47:13 +0300

    style: formatting code

--- src/shared/ui/grid/Grid.vue
+++ src/shared/ui/grid/Grid.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-[minmax(0,300px),1fr] gap-6">
-    <div class="left-block flex flex-col gap-16 mt-5">
+  <div
+    class="grid grid-cols-1 sm:grid-cols-[minmax(0,300px),1fr] gap-6 relative"
+  >
+    <div class="left-block flex flex-col gap-16 mt-5 h-full">
       <slot name="left"></slot>
     </div>
-    <div
-      id="right"
-      class="right-block flex flex-col gap-10 mt-5"
-    >
+    <div id="right" class="right-block flex flex-col gap-10 mt-5">
       <slot name="right"></slot>
     </div>
   </div>
