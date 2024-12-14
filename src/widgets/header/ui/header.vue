39bbea4ffc2eb99f6e08d571f15a7df8cae832c7
serj2626 <serj2626@mail.ru>
2024-12-14 16:48:09 +0300

    feat: updated imports

--- src/widgets/header/ui/header.vue
+++ src/widgets/header/ui/header.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { Menu } from "~/src/features/menu";
+import { Navigation } from "~/src/features/menu/navigation";
 </script>
 <template>
-  <header class="flex-col items-center flex text-center p-2 mb-3 rounded-md 
-  shadow-[5px_5px_1px_2px_rgba(0,0,0,0.115)] 
-  dark:shadow-teal-300">
-    <h1 class="font-bold my-4 text-teal-600 text-6xl dark:text-teal-400">GJ</h1>
-    <Menu />
+  <header
+    class="flex-col items-center flex text-center p-2 mb-3 rounded-md shadow-[5px_5px_1px_2px_rgba(0,0,0,0.115)] dark:shadow-teal-300"
+  >
+    <h1 class="font-bold my-2 text-teal-600 text-6xl dark:text-teal-400">GJ</h1>
+    <Navigation />
   </header>
 </template>
 <style scoped lang="scss">
