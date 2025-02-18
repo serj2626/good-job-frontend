87202bc4ec8ce7cf3e51b478590a146cd39d4d5d
serj2626 <serj2626@mail.ru>
2025-02-18 19:52:41 +0300

    feat: add import by data

--- src/features/search/ui/Search.vue
+++ src/features/search/ui/Search.vue
@@ -1,46 +1,35 @@
 <script lang="ts" setup>
-import { cities, countries } from "../model/search.data";
-const options = [
-  {
-    value: "full",
-    label: "Полный день",
-  },
-  {
-    value: "part",
-    label: "Частичная занятость",
-  },
-  {
-    value: "remote",
-    label: "Удаленная работа",
-  },
-];
+import {
+  cities,
+  countries,
+  salaries,
+  experience,
+  workSchedule,
+} from "../model/search.data";
+
+type SearchType = "resume" | "vacancy" | "employee" | "company";
+
+const { type = "resume" } = defineProps<{
+  type: SearchType;
+}>();
+
 const selected = ref("sms");
-const salaries = [
-  "Не имеет значения",
-  "От 10 000 ₽",
-  "от 25 000 ₽",
-  "от 60 000 ₽",
-  "от 100 000 ₽",
-  "от 140 000 ₽",
-  "от 180 000 ₽",
-  "от 220 000 ₽",
-];
-const experience = [
-  "Не имеет значения ",
-  "Нет опыта ",
-  "От 1 года до 3 лет ",
-  "От 3 до 6 лет ",
-  "Более 6 лет",
-];
+
+const searchTitle = computed(() => {
+  if (type === "resume") return "Поиск резюме";
+  else if (type === "vacancy") return "Поиск вакансий";
+  else if (type === "company") return "Поиск компаний";
+  else return "Поиск специалистов";
+});
 </script>
 
 <template>
   <section
     id="search"
-    class="bg-gray-100 text-center shadow-md p-3 flex flex-col gap-2"
+    class="bg-gray-100 text-center shadow-md px-3 flex flex-col gap-2"
   >
-    <h3 class="text-[#111418] text-lg font-bold px-4 pb-2 mb-4">
-      Поиск резюме
+    <h3 class="text-[#111418] text-lg font-bold mb-4">
+      {{ searchTitle }}
     </h3>
     <UInput
       color="white"
@@ -64,13 +53,13 @@ const experience = [
       <URadioGroup
         v-model="selected"
         legend="График работы"
-        :options="options"
-      />
+        :options="workSchedule"
+      /> <hr>
       <URadioGroup
         v-model="selected"
         legend="Уровень дохода"
         :options="salaries"
-      />
+      /> <hr>
       <URadioGroup
         v-model="selected"
         legend="Требуемый опыт работы"
