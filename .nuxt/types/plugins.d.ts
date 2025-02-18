6e8440a62cb30ed9939b7d102c8058595d3ba909
serj2626 <serj2626@mail.ru>
2025-02-18 19:48:01 +0300

    feat: updated settings

--- .nuxt/types/plugins.d.ts
+++ .nuxt/types/plugins.d.ts
@@ -28,7 +28,8 @@ type NuxtAppInjections =
   InjectionType<typeof import("../../node_modules/@nuxtjs/color-mode/dist/runtime/plugin.client.js")> &
   InjectionType<typeof import("../../node_modules/@nuxt/icon/dist/runtime/plugin.js")> &
   InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/dev-server-logs.js")> &
-  InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/check-if-layout-used.js")>
+  InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/check-if-layout-used.js")> &
+  InjectionType<typeof import("../../plugins/maska")>
 
 declare module '#app' {
   interface NuxtApp extends NuxtAppInjections { }
