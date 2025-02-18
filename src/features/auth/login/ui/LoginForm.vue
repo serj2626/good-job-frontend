8378232878b0ea52f916490aebed69a3787f3ddf
serj2626 <serj2626@mail.ru>
2025-02-18 19:50:08 +0300

    feat: new login form

---
+++ src/features/auth/login/ui/LoginForm.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { z } from "zod";
+import type { FormSubmitEvent } from "#ui/types";
+import BaseModal from "~/src/shared/ui/modal/BaseModal.vue";
+import PasswordInput from "~/src/shared/ui/form-field/PasswordInput.vue";
+
+const schema = z.object({
+  email: z.string().email("Некорректный email"),
+  password: z.string().min(8, "Пароль должен быть не менее 8 символов"),
+});
+
+type Schema = z.output<typeof schema>;
+
+const state = reactive({
+  email: undefined,
+  password: undefined,
+});
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  console.log(event.data);
+}
+</script>
+
+<template>
+  <BaseModal>
+    <UForm
+      :schema="schema"
+      :state="state"
+      class="space-y-4 p-6"
+      @submit="onSubmit"
+    >
+      <h4 class="text-2xl font-bold mb-4">Авторизация</h4>
+      <UFormGroup label="Почта" name="email">
+        <UInput v-model="state.email" />
+      </UFormGroup>
+
+      <UFormGroup label="Пароль" name="password">
+        <PasswordInput v-model="state.password" initType="password" />
+        <!-- <UInput v-model="state.password" type="password" /> -->
+      </UFormGroup>
+
+      <UButton size="lg" type="submit"> Войти </UButton>
+    </UForm>
+  </BaseModal>
+</template>
+
+<style lang="scss" scoped></style>
