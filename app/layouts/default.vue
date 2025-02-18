d5f5bc60043165b03f3e2b23d97bcede9d574d80
serj2626 <serj2626@mail.ru>
2025-02-18 19:48:38 +0300

    style: del other code

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,19 +2,8 @@
 import { UserMenu } from "~/src/features/menu/user-menu";
 import { Footer } from "~/src/widgets/footer";
 import { Header } from "~/src/widgets/header";
-import BaseModal from "~/src/shared/ui/modal/BaseModal.vue";
-
-const toast = useToast();
-const modal = useModal();
-const count = ref(0);
-
-function openModal() {
-  count.value += 1;
-  modal.open(BaseModal, {});
-}
 </script>
 <template>
-  <UButton class="absolute top-12 left-12" label="Reveal modal" @click="openModal" />
   <UContainer>
     <div class="flex flex-col justify-between h-full min-h-screen">
       <Header />
