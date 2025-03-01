4ab367f2fd6ce6ba454e5d119b4e0b9ab160f14e
serj2626 <serj2626@mail.ru>
2025-03-01 20:08:39 +0300

    feat: added code

--- src/features/auth/register/ui/RegisterForm.vue
+++ src/features/auth/register/ui/RegisterForm.vue
@@ -1,11 +1,38 @@
-<script lang="ts" setup>
+<script setup lang="ts">
+import { z } from 'zod'
+import type { FormSubmitEvent } from '#ui/types'
 
+const schema = z.object({
+  email: z.string().email('Invalid email'),
+  password: z.string().min(8, 'Must be at least 8 characters')
+})
+
+type Schema = z.output<typeof schema>
+
+const state = reactive({
+  email: undefined,
+  password: undefined
+})
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  // Do something with data
+  console.log(event.data)
+}
 </script>
-<template>
-  <div>
-    
-  </div>
-</template>
-<style lang="scss" scoped>
 
-</style>
+<template>
+  <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+    <h2 class="text-2xl font-bold mb-4">Регистрация</h2>
+    <UFormGroup label="Email" name="email">
+      <UInput v-model="state.email" />
+    </UFormGroup>
+
+    <UFormGroup label="Password" name="password">
+      <UInput v-model="state.password" type="password" />
+    </UFormGroup>
+
+    <UButton type="submit">
+      Создать аккаунт
+    </UButton>
+  </UForm>
+</template>
