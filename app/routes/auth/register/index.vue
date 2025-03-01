94e86ad5d353cb097a1b06e81cfb73d0d46e4c9c
serj2626 <serj2626@mail.ru>
2025-03-01 20:07:05 +0300

    feat: added import by register page

---
+++ app/routes/auth/register/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RegistrationPage } from '~/src/pages/auth-registration';
+
+
+</script>
+<template>
+    <RegistrationPage />
+</template>
+<style scoped lang="scss"></style>
