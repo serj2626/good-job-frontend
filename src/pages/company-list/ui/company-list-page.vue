ca8ff7e72118d0518563da024fb553fc4f6f18c6
serj2626 <serj2626@mail.ru>
2025-03-01 20:11:46 +0300

    feat: added import by fetch

--- src/pages/company-list/ui/company-list-page.vue
+++ src/pages/company-list/ui/company-list-page.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import { Search } from '~/src/features/search';
-import Grid from '~/src/shared/ui/grid/Grid.vue';
+import { Search } from "~/src/features/search";
+import Grid from "~/src/shared/ui/grid/Grid.vue";
 
-import type { ICompany } from '../../company-detail/model/company.type';
-import CompanyDetail from './CompanyDetail.vue';
-import Typography from '~/src/shared/ui/typography/Typography.vue';
+import type { ICompany } from "../../company-detail/model/company.type";
+import CompanyDetail from "./CompanyDetail.vue";
+import Typography from "~/src/shared/ui/typography/Typography.vue";
 
-const config = useRuntimeConfig();
-const { data: companies } = await useLazyAsyncData<ICompany[]>(
-  "companies",
-  async () => $fetch(config.public.apiUrl + `/api/companies/`)
+import { APIRoutes } from "~/src/shared/router/types";
+
+const { $apiFetch } = useNuxtApp();
+
+const { data: companies } = await useAsyncData<ICompany[]>(
+  "companies-list-data", () => $apiFetch(APIRoutes.companies.list)
 );
+
+console.log(useNuxtApp());
 </script>
 
 <template>
@@ -24,7 +28,7 @@ const { data: companies } = await useLazyAsyncData<ICompany[]>(
           class="text-[#111418] text-lg font-bold px-4 pb-2 mb-4"
           tag="h3"
         >
-        Компании
+          Компании
         </Typography>
         <CompanyDetail
           v-for="(company, index) in companies"
